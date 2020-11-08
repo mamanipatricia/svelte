5560c26420cf4cdf103e2f6f6bf033ae60cd29a1
patricia <patttymq@gmail.com>
2020-11-07 22:46:36 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Nested from "./components/Nested.svelte";
+  let name = "world!!!!!!";
+  let src = "https://catalog.redhat.com/img/svg/logo.svg";
+  let text = "this is a text, look at <strong> this </strong>"
+</script>
+
+<style>
+  p {
+    color: purple;
+    font-family: "Arial", cursive;
+    font-size: 2em;
+  }
+</style>
+
+<h1>Hello {name.toUpperCase()}</h1>
+<img {src} alt="{name} A red hat" />
+
+<p>This is a paragraph</p>
+<Nested />
+
+<p>
+  {@html text}
+</p>
